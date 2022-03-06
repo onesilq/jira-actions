f5875bb634788037961e95b4fe355e39a464fe87
ghanshyamjoshi <ghanshyam@onesliq>
2022-03-06 15:41:53 +0530

    Adding sample workflow

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ async function run() {
   try {
 
     let regex = new RegExp(`${jiraPrefix}-[0-9]+`);
-    if (jiraPrefix==null || regex.test(prName)) {
+    if (prName==null || regex.test(prName)) {
       core.setFailed("Jira Issue Key missing in PR title or description.");
       return;
     }
